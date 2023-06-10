2168aca5da31aa84c892a47c11897a0efbb6167e
QIQI <18612980825@163.com>
2023-06-10 17:47:29 +0200

    add readme and comments

--- src/GossIpServer.mjs
+++ src/GossIpServer.mjs
@@ -1,33 +1,39 @@
 import net from 'net';
 // import { Worker, isMainThread, parentPort } from 'worker_threads';
-import {MESSAGETYPE} from './MessageType.mjs';
-
+import { MESSAGETYPE } from './MessageType.mjs';
+// This class is for the GossipServer, who will process the connection of the other peers.
 export default class GossipServer {
   constructor() {
+    // Create a TCP server instance
     this.server = net.createServer();
+    // Array to store known clients
     this.KnowClients = [];
+    // Array to store unknown clients
     this.UnKnowClients = [];
+    // Bind the connection event handler to handle new client connections
     this.server.on('connection', this.handleConnection.bind(this));
   }
 
   getNetAddresses(socket) {
-    return socket.remoteAddress + ":" + socket.remotePort;
+    // Get the network address of a socket
+    return socket.remoteAddress + ':' + socket.remotePort;
   }
 
   handleConnection(socket) {
     console.log('New client connected');
     const address = this.getNetAddresses(socket);
 
+    // Check if the client is already known
     const isKnownClient = this.KnowClients.some((client) => client.address === address);
     if (!isKnownClient) {
+      // Add the new client to the known clients list
       this.KnowClients.push({ address, socket });
     }
 
     socket.on('data', (data) => {
       console.log(`Received data from client: ${data}`);
-      // console.log(`Received data from client:`);
-      // console.log(data.toString('hex'));
 
+      // Parse the received message
       const size = data.readUInt16BE(0);
       const messageType = data.readUInt16BE(2);
       const ttl = data.readUInt8(4);
@@ -41,15 +47,16 @@ export default class GossipServer {
       console.log(`Reserved: ${reserved}`);
       console.log(`Data Type: ${dataType}`);
       console.log(`Message Data: ${messageData.toString()}`);
-      console.log("\n")
+      console.log('\n');
 
-
-      if(messageType.toString == '500'){
+      // Handle different message types
+      if (messageType.toString() === '500') {
         console.log(MESSAGETYPE[messageType]);
-      }else if (messageType.toString == '531'){
+        // TODO: Handle message type 500
+      } else if (messageType.toString() === '531') {
         console.log(MESSAGETYPE[messageType]);
+        // TODO: Handle message type 531
       }
-
     });
 
     socket.on('end', () => {
@@ -59,6 +66,7 @@ export default class GossipServer {
   }
 
   broadcast(data, senderSocket) {
+    // Broadcast the data to all known clients except the sender
     this.KnowClients.forEach((client) => {
       if (client.socket !== senderSocket) {
         client.socket.write(data);
@@ -67,6 +75,7 @@ export default class GossipServer {
   }
 
   removeClient(socket) {
+    // Remove a client from the known clients list
     const index = this.KnowClients.findIndex((client) => client.socket === socket);
     if (index !== -1) {
       this.KnowClients.splice(index, 1);
@@ -74,10 +83,9 @@ export default class GossipServer {
   }
 
   start(port) {
+    // Start the server and listen on the specified port
     this.server.listen(port, () => {
       console.log(`Server is listening on port ${port}`);
     });
   }
 }
-
-
